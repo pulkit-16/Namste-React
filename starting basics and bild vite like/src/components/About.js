e043f2ec9c4f580c6362f5b971e67f85fcc20874
pulkit <takyarpulkit@gmail.com>
2024-09-10 17:37:13 +0530

    componenet complete lifecycle with api calls

--- starting basics and bild vite like/src/components/About.js
+++ starting basics and bild vite like/src/components/About.js
@@ -8,30 +8,24 @@ class About extends React.Component{
 
   constructor(props){
     super(props)
-    console.log("Parent constructor")
+    //console.log("Parent constructor")
   }
 
   componentDidMount(){
-    console.log("Parent Mounting")
+  //  console.log("Parent Mounting")
   }
 
   render(){
 
-    console.log("Parent render")
+    //console.log("Parent render")
     return(
       <div>
       <h1>About the restautant info</h1>
 
       <UserClass
-        name= {"Akshat Jaggi (class)"}
-        location= {"New Delhi"}
         email= {"akshat.jaggi@gmail.com"}
       />
-      <UserClass
-        name= {"Elon Musk(class)"}
-        location= {"Us"}
-        email= {"musk.jelon@gmail.com"}
-      />
+   
     </div>
     )
   }
--- starting basics and bild vite like/src/components/UserClass.js
+++ starting basics and bild vite like/src/components/UserClass.js
@@ -5,44 +5,47 @@ class UserClass extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            count:0,
-            count2:0
-            
-        };
-       console.log(this.props.name + "child constructor");
-    }
-
-    updateCount=()=>{
-        this.setState(
-            {
-            count :this.state.count+1,
-            count2 :this.state.count+2
+            userInfo:{
+              name :"Default" ,
+              location:"Dummy"       //   state variable initial ,, not necessary to write
+            }
         }
-    )
-        console.log("updated count" , this.state.count+1)
-        console.log("update count")
-        
-    }
-    
-    componentDidMount(){
-        console.log(this.props.name + "Child Component Mount ")
-    }
-    render(){
-        console.log("child render")
-        const{count,count2} = this.state;
 
-        const{name,location, email} = this.props;
+        console.log("child constructors")
+       
+    }
+
+    async componentDidMount(){
+        //API CALL
+     const data = await fetch("https://api.github.com/users/pulkit-16");
+     const json =  await data.json()
+     console.log(json)
+
+     this.setState({
+        userInfo:json,
+
+     })
+    }
+
+    componentDidUpdate(){
+        console.log("component updates")
+    }
+
+    componentWillUnmount(){
+        console.log("componenet unmounts")
+    }
+   
+    render(){
+        console.log("child renders")
+       const{name,location,type} =this.state.userInfo;
+        const{ email} = this.props;
+        //debugger;
         return(
             <div className='user-card'>
-            <h1>Count : {count}</h1>
-            <h1>Count : {count2}</h1>
-
-            
-            <button className="count-btn" onClick={this.updateCount}>Count Increament</button>
-
+            <h1>Type :{type}</h1>
             <h2>Name: {name}</h2>
-            <h3>Location: {location}</h3>
-            <h3>Email Id:  {email} </h3>
+            <h3>Location:{location} </h3>
+            <h3>Email Id:  {email} </h3>          {/*props*/}
         </div>
         )
         
